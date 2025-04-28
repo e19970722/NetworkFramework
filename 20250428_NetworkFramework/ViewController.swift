e1916e1142a1fc6e64e10544772b724e89bd614f
Yen Lin <yenlin@mitake.com.tw>
2025-04-28 13:40:36 +0800

    Initial Commit

---
+++ 20250428_NetworkFramework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20250428_NetworkFramework
+//
+//  Created by 金融研發一部-iOS-陳衍霖 on 2025/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
